3596070b98a9f8977b7ea2b02a479f95c87bf769
Ace <iamcodez1@gmail.com>
2020-03-31 09:29:39 +0800

    added heroku-postbuild

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.use(express.static(path.join(__dirname, 'dist/blk-design-system-angular')));
 
 app.use('/virus', virus);
 
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'dist/blk-design-system-angular/index.html')));
+
 app.listen(port, () => console.log("Server running in port: " + port));
